32db16f2bbd2854726074b8779481d99c4098474
Sriram Yeluri <yeluris@gmail.com>
2019-02-02 18:10:12 +0100

    script to check root user

---
+++ checkroot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ `whoami` != 'root' ]; then
+    echo "Executing the installer script"
+else
+    echo "Root is not allowed to execute the installer script"
+fi
+
+
+# Another way 
+if [ `id -u` -ne 0 ]; then
+        echo "Executing the installer script"
+        id
+else
+        echo "Root is not allowed to execute the installer script"
+fi
